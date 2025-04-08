7dcb9c1b82b042e5c983cf85905cef67fc4416e2
Kendaka <barengkendrick4@gmail.com>
2025-04-08 16:27:26 +0400

    initial commit for hangman word

--- src/HangmanDrawing.tsx
+++ src/HangmanDrawing.tsx
@@ -41,9 +41,35 @@ const LEFT_ARM = (
         background: "black",
         position: "absolute",
         top: "150px",
-        right: "-100PX",
-        rotate: "-30deg",  
+        right: "10px",
+        rotate: "30deg",  
+        transformOrigin: "right bottom",  
+    }} />
+)
+
+const RIGHT_LEG = (
+    <div style={{
+        width: "100px",
+        height: "10px",
+        background: "black",
+        position: "absolute",
+        top: "210px",
+        right: "-90px",
+        rotate: "60deg",  
         transformOrigin: "left bottom",  
+    }} />
+)
+
+const LEFT_LEG = (
+    <div style={{
+        width: "100px",
+        height: "10px",
+        background: "black",
+        position: "absolute",
+        top: "210px",
+        right: "0",
+        rotate: "-60deg",  
+        transformOrigin: "right bottom",  
     }} />
 )
 
@@ -55,6 +81,8 @@ export function HangmanDrawing() {
         {BODY}
         {RIGHT_ARM}
         {LEFT_ARM}
+        {RIGHT_LEG}
+        {LEFT_LEG}
         <div style={{ height: "50px", width: "10px", background: "black", position: "absolute", top: 0, right: 0}}/>
         <div style={{ height: "10px", width: "200px", background: "black", marginLeft: "120px"}}/>
         <div style={{ height: "400px", width: "10px", background: "black", marginLeft: "120px"}}/>
--- src/HangmanWord.tsx
+++ src/HangmanWord.tsx
@@ -1,5 +1,27 @@
 export function HangmanWord() {
-    return(
-        <div></div>
+    const word = "Test"
+    const guessedLetters = ["t"]
+    return (
+        <div 
+            style={{ 
+                display: "flex", 
+                gap: ".25em", 
+                fontSize: "6rem", 
+                fontWeight: "bold", 
+                textTransform: "uppercase", 
+                fontFamily: "monospace" 
+            }}
+        >
+            {word.split("").map((letter, index) => (
+                <span style={{ borderBottom: ".1em solid black"}} key={index}>
+                    <span 
+                    style={{
+                        visibility: guessedLetters.includes(letter) ? "visible" : "hidden",
+                    }}>
+                        {letter}
+                    </span>    
+                </span>
+            ))}
+        </div>
     )
 }
